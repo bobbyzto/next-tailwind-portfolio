944e9e706d3ee4f21be9f1dbf8e61f8a6de68294
Robert Weche <bobbyweche@gmail.com>
2023-01-24 16:13:07 -0800

    added nav and bio to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { BsFillMoonStarsFill, BsGithub } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import Image from "next/image";
+import deved from "../../public/dev-ed-wave.png"
 
 export default function Home() {
   return (
@@ -9,7 +17,53 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          {/* Navigation */}
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="flex text-xl font-burtons">
+              <BsGithub />
+              <a href="https://www.github.com/bobbyzto">/bobbyzto</a>
+            </h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500  text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Home Page */}
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Robert Weche
+            </h2>
+            <h3 className="text-2xl py-2">Developer and designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Freelancer providing services for programming and design content
+              needs. Join me down below and let&#39;s work together!
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+
+          {/* 3d image */}
+          <div>
+            <Image src={deved} alt={"deved"}/>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
